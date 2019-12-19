c2c2cbe8a026369db9c6995ca61de8c295146b87
ananandy0423tty <ananandy0423@icloud.com>
2019-12-19 20:38:27 +0900

    komi

--- practice/test.c
+++ practice/test.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 int main(void){
     int i = 0;
-    
+    int t = 0;
+
     while(i<=5){
         i++;
         printf("%d",i);
     }
-    printf("\n");
+    printf("\n\n");
+
+    do{
+        t++;
+        printf("%d",t);
+    }while(t<=5);
+    printf("\n\n");
     return 0;
 }
