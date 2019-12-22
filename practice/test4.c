fadcdb4783b7f7d2b0deaad01f5164eeec808322
ananandy0423tty <ananandy0423@icloud.com>
2019-12-22 15:48:32 +0900

    commit

---
+++ practice/test4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void){
+    int num;
+
+    printf("1‚©‚ç3‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+    scanf("%d",&num);
+
+    switch(num){
+        case 1:
+            printf("one\n");
+            break;
+        case 2:
+            printf("two\n");
+            break;
+        case 3:
+            printf("three\n");
+            break;
+        default:
+            printf("•s“KØ‚È’l‚Å‚·‚Ë");
+    }
+
+return 0;
+}
