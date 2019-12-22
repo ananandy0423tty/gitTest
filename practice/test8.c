aa3d8e9755d3d0b79be4e10200a16453549a98af
ananandy0423tty <ananandy0423@icloud.com>
2019-12-22 23:16:07 +0900

    commit

---
+++ practice/test8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void){
+    int num;
+    printf("•‰‚Ìƒ‹[ƒv‚Å”²‚¯‚é");
+
+    while(1){
+        printf("”’l‚ğ“ü—Í");
+        scanf("%d",&num);
+        if(num < 0){
+            break;
+        }
+
+    }
+    return 0;
+}
