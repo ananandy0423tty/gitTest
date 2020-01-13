6558cdcb35c8a8eb7b1477fe381c8d82e0cf2181
ananandy0423tty <ananandy0423@icloud.com>
2020-01-13 13:59:18 +0900

    prob4-13

---
+++ practice/prob4-13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int a;
+    srand((unsigned)time(NULL));
+    a = rand() % 10 + 1;
+    if(a>=5){
+        for(int i=1;i<=a;i++){
+            printf("š");
+        }
+    }else{
+        printf("”­¶‚µ‚½”:%d",a);
+    }
+}
