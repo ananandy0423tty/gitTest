fe5687647d46384c921bac5911f86055b6dca601
ananandy0423tty <ananandy0423@icloud.com>
2020-01-22 21:14:08 +0900

    prob6-4

--- practice/prob6-4.c
+++ practice/prob6-4.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
-void numJudge(int mun);
+int numJudge(int);
   
 void main(){
     int a;
     printf("®”’l‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢F");
     scanf("%d",&a);
     numJudge(a);
+
 }
 
-void numJudge(int num){
+int numJudge(int num){
     if(num == 0){
-        printf("0‚â‚Å");
-    }else if(num % 2 == 0){
-        printf("‹ô”‚â‚Å");
+        printf("0‚Å‚·");
+    }else if(num %2 == 0){
+        printf("‹ô”‚Å‚·");
     }else{
-        printf("Šï”‚â‚Å");
+        printf("Šï”‚Å‚·");
     }
 }
