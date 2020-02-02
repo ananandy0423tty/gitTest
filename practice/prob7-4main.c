d4bb5e4cabf85dc5e404e091005e0540bf7e8e8d
ananandy0423tty <ananandy0423@icloud.com>
2020-02-02 18:09:22 +0900

    prob7-4

---
+++ practice/prob7-4main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lation.h"
+#include "showre.h"
+ 
+
+ 
+void main(){
+    init(100);
+    showNum();
+    add(10);    //  10‰ÁŽZ
+    showNum();
+    sub(100);   //  100Œ¸ŽZ
+    showNum();
+}
+ 
