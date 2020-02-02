488f23eaca0586108f4f1547ffd01d3ac2582f58
ananandy0423tty <ananandy0423@icloud.com>
2020-02-02 18:08:30 +0900

    lation.c

---
+++ practice/lation.c
@@ -0,0 +1,20 @@
+#include "lation.h"
+
+//  ’l
+int ans;
+
+//  ‰Šú’l‚Ìİ’è
+void init(int num)
+{
+    ans = num;  //  ’l‚Ì‰Šú‰»
+}
+//  ‰ÁZ
+void add(int num)
+{
+    ans += num; //  Œ»İ‚Ì’l‚É‘«‚µZ
+}
+//  Œ¸Z
+void sub(int num)
+{
+    ans -= num; //  Œ»İ‚Ì’l‚©‚çˆø‚«Z
+}
